bcbd3182b91fc7920a2f02664a340b9be1bcc4df
mistlady <127565435+mistlady@users.noreply.github.com>
2023-04-25 23:11:42 +0100

    Add files via upload

---
+++ Age.js
@@ -0,0 +1,17 @@
+const getday = document.getElementById('day');
+const getmonth = document.getElementById(month');
+const getyear = document.getElementById('year');
+
+const letday = document.getElementById('DD');
+const letmonth = document.getElementById('MM');
+const letyear = document.getElementById('YYYY');
+
+const end = document.getElementById('end');
+
+function age(){
+    var date = new Date();
+    
+}
+
+
+end.addEventListener('click',)
